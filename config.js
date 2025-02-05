e27f16379a9bc9611c556123fb3f00c18ce12889
Casmendo <isahmusa9921@gmail.com>
2025-02-05 22:21:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || '', //Put Session-id Here
+SESSION_ID: process.env.SESSION_ID || '', //SEON-MD=LFgxHCJC#KfUInNyGu4wK5T_oTwDj1RG7ieZ3WvRP9OP4iBqbyII
 GITHUB_USERNAME: process.env.GITHUB_USERNAME || '', //Put Your Gitub Username
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || '', //Put Your Github Auth Token
 REPO_NAME: process.env.REPO_NAME || 'SEON-MD-DATABASE' //Bot Database Repo Name
